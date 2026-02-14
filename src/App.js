6f02b2de4c413594a14acc43b9d167cb94ae6e66
seray <melike_ozdemir_@outlook.com>
2026-02-14 21:31:59 +0300

    update packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home'
-import Diary from './components/Diary'
+import Home from './pages/Home'
+import Diary from './pages/Diary'
 import "./sass/index.scss"
 
 
