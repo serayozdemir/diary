d2446d69479cf85620ec8a40879f957900fc8b87
seray <melike_ozdemir_@outlook.com>
2026-02-15 01:19:53 +0300

    Tiptap Editor added but working on it

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import Navbar from "../components/Navbar"
+import { SimpleEditor } from '@/components/tiptap-templates/simple/simple-editor'
 
 export default function Diary() {
   return (
-    <div>Diary</div>
+    <div className='diary-page'>
+        <Navbar/>
+        <div className='editor'>
+            <SimpleEditor/>
+        </div>
+    </div>
   )
 }
